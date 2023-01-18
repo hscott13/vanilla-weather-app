dfc90877e1d725e1adee9526e07c004698e64ee2
hscott13 <hscott13@hotmail.co.uk>
2023-01-18 15:11:39 +0000

    Added real date and time data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 let apiKey = "0d6e6a447f07653a71842ab1529ot22b";
+
+function updateTime() {
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = days[now.getDay()];
+  let day = now.getDate();
+  let month = now.getMonth() + 1;
+  let month2digit = (month < 10 ? "0" : "") + month;
+  let year = now.getFullYear();
+  let currentDate = document.querySelector("#date-and-time");
+  currentDate.innerHTML = `${hour}:${minutes} ${weekday} ${day}/${month2digit}/${year}`;
+}
+
+updateTime();
